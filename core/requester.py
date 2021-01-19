0a956b686b4bbe2e049e61f8f3e05497bb477af0
mamaiamhacking <qi.blaze@gmail.com>
2021-01-19 20:15:31 +0800

    Add timeout for requests

--- core/requester.py
+++ core/requester.py
@@ -6,7 +6,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def requester(url, scheme, headers, origin):
     headers['Origin'] = scheme + origin
-    response = requests.get(url, headers=headers, verify=False).headers
+    response = requests.get(url, headers=headers, verify=False, timeout=30).headers
     for key, value in response.items():
         if key.lower() == 'access-control-allow-origin':
             return response
